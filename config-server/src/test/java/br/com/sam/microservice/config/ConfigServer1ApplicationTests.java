e8660a09f3b9163d6c158317f751e5ee284740fc
samuel <samuelll455@gmail.com>
2022-07-15 20:20:35 -0300

    initial commit

---
+++ config-server/src/test/java/br/com/sam/microservice/config/ConfigServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sam.microservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
